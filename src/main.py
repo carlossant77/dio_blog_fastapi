4ba780323bc498830faa08bf5ae7edf4a918595c
Carlos Santiago <josesantiag2461@gmail.com>
2025-12-23 18:04:35 -0300

    Reescreve inicialização da aplicação no main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from src.database import db_client, metadata, engine
 from src.controllers import post, auth
+from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    from src.models.post import posts
+    
+    await db_client.connect()
+    metadata.create_all(engine)
+    yield
+    await db_client.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(post.router)
 app.include_router(auth.router)
