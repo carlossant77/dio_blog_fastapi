b6802e44fe7c87d2c3a8b08ac663e0c1db62392a
Carlos Santiago <josesantiag2461@gmail.com>
2025-12-23 19:40:59 -0300

    Adiciona render-deploy para inicialização do alembic no render

---
+++ render-deploy.sh
@@ -0,0 +1,4 @@
+set -e
+
+alembic upgrade head
+uvicorn src.main:app --host 0.0.0.0 --port $PORT
--- src/main.py
+++ src/main.py
@@ -4,16 +4,6 @@ from src.controllers import post, auth
 from contextlib import asynccontextmanager
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    from src.models.post import posts
-    
-    await db_client.connect()
-    metadata.create_all(engine)
-    yield
-    await db_client.disconnect()
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(post.router)
 app.include_router(auth.router)
