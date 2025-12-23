d1513b34f7ef456d594d081cf85f4ea536572085
Carlos Santiago <josesantiag2461@gmail.com>
2025-12-23 19:51:18 -0300

    Atualiza main.py para inicialização do banco de dados.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from src.database import db_client, metadata, engine
+from src.database import db_client
 from src.controllers import post, auth
 from contextlib import asynccontextmanager
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await db_client.connect()
+    yield
+    await db_client.disconnect()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(post.router)
 app.include_router(auth.router)
